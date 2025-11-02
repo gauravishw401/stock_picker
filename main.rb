19d736760a9f8d120e0a95168c68c43713c18c88
Gaurav Vishwakarma <gauravishw401@gmail.com>
2025-11-02 09:30:47 +0530

    Add stock_picker method

---
+++ main.rb
@@ -0,0 +1,33 @@
+def stock_picker(stocks)
+
+  profits = {}
+
+  sorted_arr = stocks.sort 
+  sorted_arr.each do |min|
+    min_index = stocks.index(min)
+    previous_days = stocks.select {|element| stocks.index(element) < min_index}
+
+    next_days = stocks.difference(previous_days)
+    buy = min
+    sell = next_days.max
+    profit = sell - buy
+    profits[[buy, sell]] = profit
+
+  end
+
+  max_profit = profits.values.max
+  best_day = profits.key(max_profit).map {|element| stocks.index(element)}
+  p best_day
+
+end
+
+
+# stock_picker([17, 3, 6, 9, 15,  8, 1, 10])
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+
+# stock_picker([15,4,5,3,6, 9 , 10, 9])
+
+stock_picker([8, 736, 373, 6768, 7648, 87, 675])
+
+
